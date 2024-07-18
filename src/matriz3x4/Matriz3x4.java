d8ca43ff4fda06e64f0b13703913517a2f120ca4
souajenni <jennifervialli1@gmail.com>
2024-07-18 07:50:17 -0300

    Atividade de matriz 3x4 fornecidos pelo usuário

---
+++ src/matriz3x4/Matriz3x4.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matriz3x4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Daniel Dória
+ */
+public class Matriz3x4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][] matriz = new int [3][4];
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("Digite 12 números: ");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+        
+        System.out.println("\nSua matriz: ");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println( );
+        }
+        
+        scanner.close();
+    }
+    
+}
